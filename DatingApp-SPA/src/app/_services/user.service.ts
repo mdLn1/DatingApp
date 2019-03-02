867a20ced81727dd033198d9a4831e5d9922afec
mdln97 <madalinc.preda@gmail.com>
2019-03-02 22:24:00 +0000

    added likes functionality

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -15,7 +15,7 @@ export class UserService {
 
 constructor(private http: HttpClient) { }
 
-  getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>> {
+  getUsers(page?, itemsPerPage?, userParams?, likesParam?): Observable<PaginatedResult<User[]>> {
     const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
 
     let params = new HttpParams();
@@ -32,6 +32,13 @@ constructor(private http: HttpClient) { }
       params = params.append('orderBy', userParams.orderBy);
     }
 
+    if (likesParam === 'Likers') {
+      params = params.append('likers', 'true');
+    }
+
+    if (likesParam === 'Likees') {
+      params = params.append('likees', 'true');
+    }
 
     return this.http.get<User[]>(this.baseUrl + 'users', { observe: 'response', params})
       .pipe(
@@ -59,5 +66,9 @@ constructor(private http: HttpClient) { }
 
   deletePhoto(userId: number, id: number) {
     return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
+  }
+
+  sendLike(id: number, recipientId: number) {
+    return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
   }
 }
